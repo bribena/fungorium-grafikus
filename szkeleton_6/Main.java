f63b5cc7ff71f82b1de65c5f3e1ae5715bf025e1
Horváth Adrián Márk <hadri0321@gmail.com>
2025-03-19 16:29:35 +0100

    Alap szerkezet a use-caseknek

---
+++ szkeleton_6/Main.java
@@ -0,0 +1,7 @@
+package szkeleton_6;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Use-casek:\n");
+    }
+}
---
+++ szkeleton_6/usecasek.java
@@ -0,0 +1,35 @@
+package szkeleton_6;
+
+public class usecasek {
+    static void tekton_tor() {
+        System.out.println("usecase1");
+    }
+
+    static void rovart_mozgat() {
+        System.out.println("usecase1");
+    }
+
+    static void rovart_etet() {
+        System.out.println("usecase1");
+    }
+
+    static void gombafonalat_vag() {
+        System.out.println("usecase1");
+    }
+
+    static void gombafonalat_noveszt() {
+        System.out.println("usecase1");
+    }
+
+    static void gombatestet_noveszt() {
+        System.out.println("usecase1");
+    }
+
+    static void gombatestet_fejleszt() {
+        System.out.println("usecase1");
+    }
+
+    static void sporat_szor() {
+        System.out.println("usecase1");
+    }
+}
