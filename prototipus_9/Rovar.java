b0dac9022bd6c6fcf96603e49dec1d0c9dd51eaf
AgarFire <kulbencz.levente2004@gmail.com>
2025-04-25 15:17:54 +0200

    par hianyzo rovar metodus

--- prototipus_9/Rovar.java
+++ prototipus_9/Rovar.java
@@ -1,8 +1,14 @@
 package prototipus_9;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Rovar implements Entitás {
     private Rovarfaj faj;
     private int[] hatások;
+
+    private Random random = new Random();
 
     public boolean frissítés()
     {
@@ -15,6 +21,29 @@ public class Rovar implements Entitás {
 
     public void spóraFeldolgozás(int mennyi) {
         //a paraméterként megadott mennyiségű spórát feldolgozza, tehát hatást rak a rovarra
+
+        List<Integer> sorsolhato = new ArrayList<>(); // ez ahhoz kell, hogy az elso 4 garantaltan kulonbozo legyen
+
+        for (int i = 0; i < 4; i++)
+        {
+            sorsolhato.add(i);
+        }
+
+        for (int i = 0; i < mennyi; i++)
+        {
+            if (i < 4)
+            {
+                int roll = random.nextInt(sorsolhato.size()); // elvileg mukodik
+                
+                hatások[sorsolhato.get(roll-1)] += 1; // idk mennyivel kene novelni
+                sorsolhato.remove(roll); // kivesszuk a listabol hogy a tobbibol sorsoljon a kovetkezo ciklusban
+            }
+            else
+            {
+                int roll = random.nextInt(4); // itt mar full random
+                hatások[roll-1] += 1; // idk mennyivel kene novelni
+            }
+        }
     }
 
     public boolean hatásaAlatt(int idx){
@@ -34,6 +63,7 @@ public class Rovar implements Entitás {
         //a paraméterként kapott tektonrészen lévő,
         // szintén paraméterként kapott fonalat végja el,
         // vagyis a fonalnak nem lesznek szomszédai
+
         return true;
     }
 
@@ -43,10 +73,13 @@ public class Rovar implements Entitás {
     }
 
     public void eszik(Tektonrész t){
+
     }
 
     public void mozog(Tektonrész t1, Tektonrész t2){
         //a paraméterként kapott első tektonrészről a második tektonrészre mozgatja a rovart
+        t1.entitásTörlés(this);
+        t2.entitásHozzáadás(this);
     }
 
     public boolean gyenge(){
