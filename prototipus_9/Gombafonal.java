42bb283de53dd49d07d2864c724eba27a61148ee
bribena <bence.bihari@edu.bme.hu>
2025-04-26 20:37:36 +0000

    Még a gombatestes cumót meg kell javítani, de biztos, hogy nem most piszkálom meg.

--- prototipus_9/Gombafonal.java
+++ prototipus_9/Gombafonal.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 public class Gombafonal implements Entitás {
     private int szakadt = 0;
-    private List<Gombafonal> kapcsolodoFonalak = new ArrayList<>();
+    /**
+     * [0]: Fel
+     * [1]: Jobbra
+     * [2]: Le
+     * [3]: Balra
+     */
+    private Gombafonal[] kapcsolodoFonalak = new Gombafonal[4];
     private List<Gombatest> gombaTestek = new ArrayList<>();
     private Gombafaj faj;
 
     public Gombafonal(Gombafonal forras)
     {
         faj = forras.getFaj();
-        kapcsolodoFonalak.add(forras);
     }
 
     @Override
@@ -34,11 +39,49 @@ public class Gombafonal implements Entitás {
         return false;
     }
 
-    public boolean gombafonalatNöveszt(Tektonrész honnan, Tektonrész hova) // a honnan-t nem tudom hogyan alkalmazzam
+    public boolean gombafonalatNöveszt(Tektonrész honnan, Tektonrész hova, Fungorium fungorium) // a honnan-t nem tudom hogyan alkalmazzam
     {
         if (!kapcsolódikGombatesthez()) // csak akkor noveszthet ha van kapcsolodo gombatest
         {
             return false;
+        }
+
+        int[] honnanKoor = fungorium.getTektonrészKoordináta(honnan);
+        int[] hovaKoor = fungorium.getTektonrészKoordináta(hova);
+
+        // Összenövesztés
+        List<Gombafonal> fonalak = hova.getGombafonalak();
+        if (!fonalak.isEmpty()) {
+            for (Gombafonal fonal : fonalak) {
+                if (fonal.faj == faj) {
+                    if (honnanKoor[0] - 1 == hovaKoor[0]) {
+                        // Balra új
+                        kapcsolodoFonalak[3] = fonal;
+                        fonal.kapcsolodoFonalak[1] = this;
+                    }
+                    else if (honnanKoor[0] + 1 == hovaKoor[0]) {
+                        // Jobbra új
+                        kapcsolodoFonalak[1] = fonal;
+                        fonal.kapcsolodoFonalak[3] = this;
+                    }
+                    else if (honnanKoor[1] - 1 == hovaKoor[1]) {
+                        // Fel új
+                        kapcsolodoFonalak[0] = fonal;
+                        fonal.kapcsolodoFonalak[2] = this;
+                    }
+                    else if (honnanKoor[1] + 1 == hovaKoor[1]) {
+                        // Le új
+                        kapcsolodoFonalak[2] = fonal;
+                        fonal.kapcsolodoFonalak[0] = this;
+                    }
+                    else {
+                        // Nem szomszéd
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
         }
 
         Gombafonal fonal = new Gombafonal(this);
@@ -50,7 +93,31 @@ public class Gombafonal implements Entitás {
 
         if (hova.entitásHozzáadás(fonal)) // a tektonresz eldonti, hogy lehetseges-e a novesztes
         {
-            kapcsolodoFonalak.add(fonal);
+            if (honnanKoor[0] - 1 == hovaKoor[0]) {
+                // Balra új
+                kapcsolodoFonalak[3] = fonal;
+                fonal.kapcsolodoFonalak[1] = this;
+            }
+            else if (honnanKoor[0] + 1 == hovaKoor[0]) {
+                // Jobbra új
+                kapcsolodoFonalak[1] = fonal;
+                fonal.kapcsolodoFonalak[3] = this;
+            }
+            else if (honnanKoor[1] - 1 == hovaKoor[1]) {
+                // Fel új
+                kapcsolodoFonalak[0] = fonal;
+                fonal.kapcsolodoFonalak[2] = this;
+            }
+            else if (honnanKoor[1] + 1 == hovaKoor[1]) {
+                // Le új
+                kapcsolodoFonalak[2] = fonal;
+                fonal.kapcsolodoFonalak[0] = this;
+            }
+            else {
+                // Nem szomszéd
+                return false;
+            }
+
             return true;
         }
         else
@@ -92,11 +159,6 @@ public class Gombafonal implements Entitás {
         return false;
     }
 
-    public void fonalÖsszenövesztés(Gombafonal masik)
-    {
-        kapcsolodoFonalak.add(masik); // feltetelezem hogy a masik-hoz mar hozza van kotve ez a fonal
-    }
-
     public void gombatestHozzáadása(Gombatest t)
     {
         gombaTestek.add(t);
@@ -107,7 +169,7 @@ public class Gombafonal implements Entitás {
         return gombaTestek.size() > 0;
     }
 
-    public List<Gombafonal> getKapcsolodoFonalak()
+    public Gombafonal[] getKapcsolodoFonalak()
     {
         return kapcsolodoFonalak;
     }
