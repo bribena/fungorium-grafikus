288e3e8e222150a5ba2407dec05cf1e9dc049890
AgarFire <kulbencz.levente2004@gmail.com>
2025-04-26 11:16:08 +0200

    tesztfuggvenyek deklaracioja

--- prototipus_9/Main.java
+++ prototipus_9/Main.java
@@ -5,9 +5,94 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static void fungorium_torol(int azonosito)
+    private static void futtat(String file)
     {
 
+    }
+
+    private static void load()
+    {
+
+    }
+
+    private static void fungorium_torol(int id)
+    {
+
+    }
+
+    private static void fungorium_tor(int tektonId)
+    {
+
+    }
+
+    private static void gombasz_uj(int id)
+    {
+
+    }
+
+    private static void rovarasz_uj(int id)
+    {
+
+    }
+
+    private static void tekton_szomszed(int t1_id, int t2_id)
+    {
+
+    }
+
+    private static void gomba_noveszt(int gombaId, int jatekosId, int tektonId)
+    {
+
+    }
+
+    private static void gomba_fejleszt(int gombaId)
+    {
+
+    }
+
+    private static void gomba_szoras(int gombaId)
+    {
+
+    }
+
+    private static void gombaf_szakit(int forrasId, int celId)
+    {
+
+    }
+
+    private static void gombaf_noveszt(int fonalId, int forrasId, int celId)
+    {
+
+    }
+
+    private static void gombaf_rovarbol(int fonalId, int jatekosId, int rovarId)
+    {
+
+    }
+
+    private static void spora_szam(int ertek)
+    {
+
+    }
+
+    private static void rovar_hatas(int rovarId, String hatas, int ertek)
+    {
+
+    }
+
+    private static void rovar_vag(int rovarId, int forrasId, int celId)
+    {
+
+    }
+
+    private static void rovar_mozog(int rovarId, int forrasId, int celId)
+    {
+
+    }
+
+    private static void rovar_eszik(int rovarId, int tektonId)
+    {
+        
     }
 
     public static void main(String[] args) {
