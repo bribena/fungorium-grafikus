c3bcae76bd75e63ce1dda0cf947dfa3fff5d9cbc
Sali Nóra <salinora2005@gmail.com>
2025-05-20 12:37:57 +0200

    szintaktika

--- fungorium/Models/Játék.java
+++ fungorium/Models/Játék.java
@@ -65,35 +65,35 @@ public class Játék {
     }
 
     public String getWinnerGombasz() {
-    int maxPont = -1;
-    int győztesIndex = -1;
+        int maxPont = -1;
+        int győztesIndex = -1;
 
-    for (int i = 0; i < 4; i++) {
-        Gombafaj faj = ((Gombász) játékosok.get(i)).getKezeltFaj();
-        int pont = Gombafonal.getGombatestSzám(faj);
-        if (pont > maxPont) {
-            maxPont = pont;
-            győztesIndex = i;
+        for (int i = 0; i < 4; i++) {
+            Gombafaj faj = ((Gombász) játékosok.get(i)).getKezeltFaj();
+            int pont = Gombafonal.getGombatestSzám(faj);
+            if (pont > maxPont) {
+                maxPont = pont;
+                győztesIndex = i;
+            }
         }
+
+        // Nincs ellenőrzés, simán visszaadjuk a győztes nevét
+        return játékosok.get(győztesIndex).getName();
     }
 
-    // Nincs ellenőrzés, simán visszaadjuk a győztes nevét
-    return játékosok.get(győztesIndex).getName();
-}
+    public String getWinnerRovarasz() {
+        int maxPont = -1;
+        int győztesIndex = -1;
 
-public String getWinnerRovarasz() {
-    int maxPont = -1;
-    int győztesIndex = -1;
-
-    for (int i = 4; i < 8; i++) {
-        Rovarfaj faj = ((Rovarász) játékosok.get(i)).getKezeltFaj();
-        int pont = Rovar.getTápérték(faj);
-        if (pont > maxPont) {
-            maxPont = pont;
-            győztesIndex = i;
+        for (int i = 4; i < 8; i++) {
+            Rovarfaj faj = ((Rovarász) játékosok.get(i)).getKezeltFaj();
+            int pont = Rovar.getTápérték(faj);
+            if (pont > maxPont) {
+                maxPont = pont;
+                győztesIndex = i;
+            }
         }
-    }
 
-    return játékosok.get(győztesIndex).getName();
-}
+        return játékosok.get(győztesIndex).getName();
+    }
 }
